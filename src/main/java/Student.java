b1c6d69d3a8a10eb47008ded9177f9621f2add25
Jay Arredondo <jearredondo13@gmail.com>
2020-06-30 15:09:39 -0500

    finished with part one of the exercise

---
+++ src/main/java/Student.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Constructor
+
+    public Student(String name, long id){
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+
+    }
+
+    // Methods
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double averageGrade = 0;
+        for (Integer grade : grades) {
+            averageGrade += grade;
+        }return averageGrade/grades.size();
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+    private Student student;
+
+    @Before
+    public void setUp(){
+        this.student = new Student("Jay", 1234567);
+    }
+
+    @Test
+    public void testCheckConstructor(){
+        assertEquals(student.getName(), "Jay");
+        assertEquals(student.getId(), 1234567);
+        assertNotNull(student.getGrades());
+    }
+
+    @Test
+    public void testAddGrade(){
+        student.addGrade(100);
+
+
+        assertEquals(1, student.getGrades().size());
+        assertSame(100, student.getGrades().get(0));
+    }
+
+    @Test
+    public void testGetGradeAverage(){
+        student.addGrade(100);
+        student.addGrade(75);
+        student.addGrade(50);
+        double average = student.getGradeAverage();
+
+        // test if grades are there and if they match the index.
+        assertEquals(3, student.getGrades().size());
+        assertSame(100, student.getGrades().get(0));
+        assertSame(75, student.getGrades().get(1));
+        assertSame(50, student.getGrades().get(2));
+
+        // test output
+        assertEquals(75, average, 0);
+
+    }
+}
